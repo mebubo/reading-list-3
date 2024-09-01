0c2b1e2b95f959f542867a6814738a49f22cc2e7
Sergei Dolgov <dolgovs@gmail.com>
2024-09-01 17:07:37 +0200

    Move the extension source code to a subdir

--- frontend/esbuild.js
+++ frontend/esbuild.js
@@ -3,15 +3,16 @@ import * as esbuild from 'esbuild'
 esbuild
   .build({
     entryPoints: [
-      "./src/background.ts",
-      "./src/content.ts",
-      "./src/popup.tsx",
+      "./src/extension/background.ts",
+      "./src/extension/content.ts",
+      "./src/extension/popup.tsx",
     ],
     bundle: true,
     minify: false,
     sourcemap: process.env.NODE_ENV !== "production",
     target: ["chrome120", "firefox110"],
-    outdir: "./public/extension/",
+    outdir: "public",
+    outbase: "src",
     define: {
       "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`
     }
